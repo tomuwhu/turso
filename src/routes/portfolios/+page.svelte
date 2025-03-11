ce6de2299fe80cf58bfb3dc1dd5e8a9889bc4eaa
Dr. Németh Tamás <tomuwhu@gmail.com>
2025-03-11 20:30:25 +0100

    .

--- src/routes/portfolios/+page.svelte
+++ src/routes/portfolios/+page.svelte
@@ -1,10 +1,8 @@
 <script>
   let { data } = $props();
-  let selectedgroup = $state(1);
 </script>
 <h2>Regisztrált portfóliók listája</h2>
 
-
 {#each data.users as user}
-  <a href={user.projectURL} target="_blank">{user.name}</a>
+  <a href={user.projectURL} target="_blank">{user.name}</a> ({user.email}) <br>
 {/each}

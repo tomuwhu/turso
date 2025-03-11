c19bcee3a9c04d9f6f3cd4d547c4f106913223aa
Dr. Németh Tamás <tomuwhu@gmail.com>
2025-03-11 15:50:32 +0100

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to SvelteKit with Turso, Lucia and Drizzle</h1>
+<a href="https://sveltekitwithturso.netlify.app/">This Project on Netlify</a>
+<a href="https://github.com/sveltekitwithturso/sveltekitwithturso">This Project on GitHub</a>
